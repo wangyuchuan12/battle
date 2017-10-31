e9a3b85da7081a63d92408d4204cc9b7b555aab5
System Administrator <root@dawang.local>
2017-10-31 12:38:02 +0800

    commit

---
+++ battle-api/src/main/java/com/battle/api/MsgApi.java
@@ -0,0 +1,23 @@
+package com.battle.api;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value="/api/battle/msg/")
+public class MsgApi {
+	@RequestMapping(value="info")
+	@ResponseBody
+	public String send(HttpServletRequest httpServletRequest){
+		String signature = httpServletRequest.getParameter("signature");
+	    String timestamp = httpServletRequest.getParameter("timestamp");
+	    String nonce = httpServletRequest.getParameter("nonce");
+	    String echostr= httpServletRequest.getParameter("echostr");
+	    
+	    System.out.println("signature:"+signature+",timestamp:"+timestamp+",nonce:"+nonce+",echostr:"+echostr);
+	    return echostr;
+	}
+}
