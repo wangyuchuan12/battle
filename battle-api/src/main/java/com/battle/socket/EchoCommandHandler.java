29efcfa035875e04a8ce38ca0b8ec666197ac9f5
System Administrator <root@dawang.local>
2017-12-19 13:37:36 +0800

    测试

--- battle-api/src/main/java/com/battle/socket/EchoCommandHandler.java
+++ battle-api/src/main/java/com/battle/socket/EchoCommandHandler.java
@@ -2,13 +2,17 @@ package com.battle.socket;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+import org.apache.catalina.connector.RequestFacade;
 
 @Controller
 public class EchoCommandHandler {
-	
+
 	@RequestMapping("/websocket/send")
 	public Object test()throws Exception{
+		
+		System.out.println("............................sss");
+		new RequestFacade(null).upgrade(null);
 		return null;
 	}
 }
