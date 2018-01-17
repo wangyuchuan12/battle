c9e90e086f5374c64ca9cb7d4692d990733a63a1
System Administrator <root@dawang.local>
2018-01-17 20:40:47 +0800

    battleDanUser加上缓存

--- battle-service/src/main/java/com/battle/service/BattleDanUserService.java
+++ battle-service/src/main/java/com/battle/service/BattleDanUserService.java
@@ -1,26 +1,84 @@
 package com.battle.service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.battle.dao.BattleDanUserDao;
 import com.battle.domain.BattleDanUser;
+import com.battle.domain.BattlePeriodMember;
+import com.wyc.common.service.RedisService;
 
 @Service
 public class BattleDanUserService {
+	
+	private static final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+	
+	private final String  BATTLE_DAN_USERS_BY_USERID_POINTID_KEY = "key1";
+	
+	final static Logger logger = LoggerFactory.getLogger(BattleDanUserService.class);
+	@Autowired
+	private RedisService redisService;
 
 	@Autowired
 	private BattleDanUserDao battleDanUserDao;
+	
+	public void saveToRedisByUserIdAndPointId(String userId,String pointId,List<BattleDanUser> battleDanUsers){
+		try{
+			readWriteLock.writeLock().lock();
+			String key = BATTLE_DAN_USERS_BY_USERID_POINTID_KEY;
+			key = key+"_"+userId+"_"+pointId;
+			redisService.setList(key, battleDanUsers,BattleDanUser.class);
+		}catch(Exception e){
+			logger.error("缓存List<BattleDanUser>到redis出错");
+		}finally{
+			readWriteLock.writeLock().unlock();
+		}
+		
+	}
+	
+	public List<BattleDanUser> findBattleDanUsersByUserIdAndPointId(String userId,String pointId){
+		
+		try{
+			readWriteLock.readLock().lock();
+			String key =  BATTLE_DAN_USERS_BY_USERID_POINTID_KEY;;
+			key = key+"_"+userId+"_"+pointId;
+			List<BattleDanUser> battleDanUsers = redisService.getList(key);
+			Collections.sort(battleDanUsers, new Comparator<BattleDanUser>() {
+				public int compare(BattleDanUser battleDanUser, BattleDanUser battleDanUser2) {  
+	                return battleDanUser.getLevel().compareTo(battleDanUser2.getLevel());
+	            }  
+			});
+			return battleDanUsers;
+		}catch(Exception e){
+			logger.error("从redis得到List<BattleDanUser>数据出错");
+		}finally{
+			readWriteLock.readLock().unlock();
+			
+		}
+		return null;
+		
+	}
 
 	public List<BattleDanUser> findAllByUserIdAndPointIdOrderByLevelAsc(String userId, String pointId) {
 		
-		List<BattleDanUser> battleDanUsers = battleDanUserDao.findAllByUserIdAndPointId(userId,pointId);
+		List<BattleDanUser> battleDanUsers = findBattleDanUsersByUserIdAndPointId(userId, pointId);
+		
+		if(battleDanUsers!=null){
+			return battleDanUsers;
+		}
+				
+		battleDanUsers = battleDanUserDao.findAllByUserIdAndPointId(userId,pointId);
 		
 		Collections.sort(battleDanUsers, new Comparator<BattleDanUser>() {
 			public int compare(BattleDanUser battleDanUser, BattleDanUser battleDanUser2) {  
@@ -28,7 +86,10 @@ public class BattleDanUserService {
             }  
 		});
 		
+		saveToRedisByUserIdAndPointId(userId, pointId, battleDanUsers);
+		
 		return battleDanUsers;
+		
 	}
 
 	public void add(BattleDanUser battleDanUser) {
@@ -43,11 +104,21 @@ public class BattleDanUserService {
 	}
 
 	public void update(BattleDanUser battleDanUser) {
-		
 		battleDanUser.setUpdateAt(new DateTime());
-		
 		battleDanUserDao.save(battleDanUser);
+		List<BattleDanUser> battleDanUsers = findAllByUserIdAndPointIdOrderByLevelAsc(battleDanUser.getUserId(), battleDanUser.getPointId());
 		
+		List<BattleDanUser> battleDanUsers2 = new ArrayList<>();
+		
+		for(BattleDanUser battleDanUser2:battleDanUsers){
+			if(battleDanUser2.getId().equals(battleDanUser.getId())){
+				battleDanUsers2.add(battleDanUser);
+			}else{
+				battleDanUsers2.add(battleDanUser2);
+			}
+		}
+		
+		saveToRedisByUserIdAndPointId(battleDanUser.getUserId(), battleDanUser.getPointId(), battleDanUsers2);
 	}
 
 	public BattleDanUser findOne(String id) {
