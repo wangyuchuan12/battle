b7546576d6a87e6aab27ec769c1d48987653a9ff
System Administrator <root@dawang.local>
2018-01-30 00:52:58 +0800

    test

--- battle-dao/src/main/java/com/battle/dao/BattleMemberLoveCoolingDao.java
+++ battle-dao/src/main/java/com/battle/dao/BattleMemberLoveCoolingDao.java
@@ -1,16 +1,13 @@
 package com.battle.dao;
 
 import javax.persistence.LockModeType;
-
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 
 import com.battle.domain.BattleMemberLoveCooling;
 
 public interface BattleMemberLoveCoolingDao extends CrudRepository<BattleMemberLoveCooling, String>{
-
-	@Cacheable(value="userCache",keyGenerator="sessionKeyGenerator")
+	
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	BattleMemberLoveCooling findOneByBattleMemberId(String battleMemberId);
 
