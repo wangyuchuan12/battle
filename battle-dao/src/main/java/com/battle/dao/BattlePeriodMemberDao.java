dd202eacec8c43604264852847076dce6ec249c8
System Administrator <root@dawang.local>
2018-01-31 00:33:10 +0800

    battlePeriodmembers

--- battle-dao/src/main/java/com/battle/dao/BattlePeriodMemberDao.java
+++ battle-dao/src/main/java/com/battle/dao/BattlePeriodMemberDao.java
@@ -20,7 +20,7 @@ public interface BattlePeriodMemberDao extends CrudRepository<BattlePeriodMember
 	BattlePeriodMember findOneByBattleIdAndBattleUserIdAndPeriodIdAndRoomIdAndIsDel(String battleId, String battleUserId,
 			String periodId,String roomId,Integer isDel);
 
-	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
+	//@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	List<BattlePeriodMember> findAllByBattleIdAndPeriodIdAndRoomIdAndStatusInAndIsDel(String battleId, String periodId, String roomId,List<Integer> statuses,Integer isDel,Pageable pageable);
 
 	BattlePeriodMember findOneByRoomIdAndBattleUserIdAndIsDel(String roomId,String battleUserId,Integer isDel);
@@ -28,14 +28,13 @@ public interface BattlePeriodMemberDao extends CrudRepository<BattlePeriodMember
 	@Cacheable(value="userCache",keyGenerator="sessionKeyGenerator")
 	BattlePeriodMember findOneByRoomIdAndUserIdAndIsDel(String roomId,String battleUserId,Integer isDel);
 	
-	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	@Query(value="select count(*)+1 from com.battle.domain.BattlePeriodMember bm where bm.roomId=:roomId and bm.score>:score ")
 	Long rank(@Param("roomId")String roomId,@Param("score")Integer score);
 
-	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
+	//@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	List<BattlePeriodMember> findAllByBattleIdAndPeriodIdAndRoomIdOrderByCreateAtAsc(String battleId, String periodId, String roomId);
 
-	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
+	//@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	List<BattlePeriodMember> findAllByBattleIdAndPeriodIdAndRoomId(String battleId, String periodId, String roomId,
 			Pageable pageable);
 	
@@ -43,7 +42,7 @@ public interface BattlePeriodMemberDao extends CrudRepository<BattlePeriodMember
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	BattlePeriodMember findOne(String id);
 
-	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
+	//@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	@Query("from com.battle.domain.BattlePeriodMember bpm where bpm.battleId=:battleId and bpm.periodId=:periodId and bpm.roomId=:roomId and bpm.status in (:statuses) and bpm.isDel=:isDel and EXISTS(select id from com.battle.domain.BattleRoomGroupMember brgm where brgm.groupId=:groupId and brgm.userId=bpm.userId) ")
 	List<BattlePeriodMember> findAllByBattleIdAndPeriodIdAndRoomIdAndStatusInAndGroupIdAndIsDel(@Param("battleId")String battleId,
 			@Param("periodId")String periodId, @Param("roomId")String roomId,@Param("statuses")List<Integer> statuses, @Param("groupId")String groupId, @Param("isDel") int isDel, Pageable pageable);
