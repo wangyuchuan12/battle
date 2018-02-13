81fa9160dcf84d2f9339469b219b597409e78a39
System Administrator <root@dawang.local>
2018-02-13 19:53:04 +0800

    commit

--- battle-api/src/main/java/com/battle/api/BattleRankDanApi.java
+++ battle-api/src/main/java/com/battle/api/BattleRankDanApi.java
@@ -43,5 +43,7 @@ public class BattleRankDanApi {
 		List<BattleAccountResult> battleAccountResults = battleAccountResultService.findAllByUserFrendUserId(userInfo.getId());
 		
 		
+		return null;
+		
 	}
 }
