4cd24441e4de712d33dcd2335fc9aaeb0b175ab6
System Administrator <root@dawang.local>
2018-02-13 20:30:46 +0800

    userInfo

--- battle-dao/src/main/java/com/battle/dao/BattleAccountResultDao.java
+++ battle-dao/src/main/java/com/battle/dao/BattleAccountResultDao.java
@@ -12,7 +12,7 @@ public interface BattleAccountResultDao extends CrudRepository<BattleAccountResu
 
 	BattleAccountResult findOneByUserId(String userId);
 
-	@Query(value="from com.battle.domain.BattleAccountResult bar where exists(select id from com.battle.domain.UserFriend uf where uf.userId=:userId and uf.friendUserId=bar.userId) order by bar.level asc")
+	@Query(value="from com.battle.domain.BattleAccountResult bar where exists(select id from com.battle.domain.UserFriend uf where bar.userId=:userId and uf.friendUserId=bar.userId) order by bar.level asc")
 	List<BattleAccountResult> findAllByUserFrendUserId(@Param("userId")String userId);
 
 }
