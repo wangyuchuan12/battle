38b5bfebcd4db1541ff7982f5359e26659d6ea33
System Administrator <root@dawang.local>
2018-02-21 22:37:37 +0800

    commit

--- battle-dao/src/main/java/com/battle/dao/BattleDrawItemDao.java
+++ battle-dao/src/main/java/com/battle/dao/BattleDrawItemDao.java
@@ -2,12 +2,14 @@ package com.battle.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.battle.domain.BattleDrawItem;
 
 public interface BattleDrawItemDao extends CrudRepository<BattleDrawItem, String>{
 
+	@Query("from com.battle.domain.BattleDrawItem bdt order by level asc")
 	List<BattleDrawItem> findAllOrderByLevelAsc();
 
 }
