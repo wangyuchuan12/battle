309d8efa66667357677c063a7b54cadee70b77e9
System Administrator <root@dawang.local>
2018-03-05 12:52:58 +0800

    commit

--- battle-api/src/main/java/com/battle/api/TestApi.java
+++ battle-api/src/main/java/com/battle/api/TestApi.java
@@ -1,8 +1,5 @@
 package com.battle.api;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.battle.service.other.CustomService;
 import com.battle.service.other.MessageHandleService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wyc.common.domain.vo.ResultVo;
 
 @Controller
 @RequestMapping(value="/api/test")
@@ -20,6 +16,10 @@ public class TestApi {
 
 	@Autowired
 	private MessageHandleService messageHandleService;
+	
+	@Autowired
+	private CustomService customService;
+	
 	@ResponseBody
 	@RequestMapping(value="test")
 	public Object test(HttpServletRequest httpServletRequest)throws Exception{
@@ -28,31 +28,12 @@ public class TestApi {
 		
 		String smgtype = httpServletRequest.getParameter("smgtype");
 		
-		if(smgtype.equals("text")){
-			String content = httpServletRequest.getParameter("content");
-			
-			Map<String, Object> data = new HashMap<>();
-			
-			data.put("content", content);
-			return messageHandleService.send(toUser, smgtype, data);
-			
-		}else if(smgtype.equals("image")){
-			String mediaId = httpServletRequest.getParameter("mediaId");
-			Map<String, Object> content = new HashMap<>();
-			content.put("media_id", mediaId);
-			return messageHandleService.send(toUser, smgtype, content);
-		}else if(smgtype.equals("link")){
-			String title = httpServletRequest.getParameter("title");
-			String description = httpServletRequest.getParameter("description");
-			String url = httpServletRequest.getParameter("url");
-			String thumbUrl = httpServletRequest.getParameter("thumbUrl");
-			Map<String, Object> content = new HashMap<>();
-			content.put("title", title);
-			content.put("description", description);
-			content.put("url", url);
-			content.put("thumb_url", thumbUrl);
-			return messageHandleService.send(toUser, smgtype, content);
-		}
+		String title = httpServletRequest.getParameter("title");
+		String description = httpServletRequest.getParameter("description");
+		String url = httpServletRequest.getParameter("url");
+		String thumbUrl = httpServletRequest.getParameter("thumbUrl");
+		
+		customService.sendLinkMsg(toUser, smgtype, title, description, url, thumbUrl);
 		
 		return null;
 	}
