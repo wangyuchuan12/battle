8bacfed57e88b57d9749604e3cd4d3d10abf75ba
System Administrator <root@dawang.local>
2018-03-09 11:12:43 +0800

    commit

--- battle-dao/src/main/java/com/battle/dao/BattleStepIndexModelDao.java
+++ battle-dao/src/main/java/com/battle/dao/BattleStepIndexModelDao.java
@@ -1,19 +1,18 @@
 package com.battle.dao;
-
 import java.util.List;
-
-import org.springframework.cache.annotation.Cacheable;
+import javax.persistence.QueryHint; 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 
 import com.battle.domain.BattleStepIndexModel;
 
 public interface BattleStepIndexModelDao extends CrudRepository<BattleStepIndexModel, String>{
 
-	@Cacheable(value="userCache")
+	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	List<BattleStepIndexModel> findAll(Pageable pageable);
 
-	@Cacheable(value="userCache")
+	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	List<BattleStepIndexModel> findAllByModelId(String modelId);
 
 }
--- battle-dao/src/main/java/com/battle/dao/BattleStepModelDao.java
+++ battle-dao/src/main/java/com/battle/dao/BattleStepModelDao.java
@@ -1,20 +1,18 @@
 package com.battle.dao;
-
-import java.util.List;
-
-import org.springframework.cache.annotation.Cacheable;
+import javax.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 
 import com.battle.domain.BattleStepModel;
 
 public interface BattleStepModelDao extends CrudRepository<BattleStepModel, String>{
 	
-	@Cacheable(value="userCache") 
+	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	Page<BattleStepModel> findAll(Pageable pageable);
 
-	@Cacheable(value="userCache") 
+	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	BattleStepModel findOneByCode(String code);
 
 }
