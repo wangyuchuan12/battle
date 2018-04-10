5e8fac0d422a75a3d27a4ebf5448b63df38d979f
System Administrator <root@dawang.local>
2018-04-10 17:16:19 +0800

    commit

--- battle-api/src/main/java/com/battle/socket/EchoCommandHandler.java
+++ battle-api/src/main/java/com/battle/socket/EchoCommandHandler.java
@@ -1,18 +1,13 @@
-//package com.battle.socket;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import org.apache.catalina.connector.RequestFacade;
-//
-//@Controller
-//public class EchoCommandHandler {
-//
-//	@RequestMapping("/websocket/send")
-//	public Object test()throws Exception{
-//		
-//		System.out.println("............................sss");
-//		new RequestFacade(null).upgrade(null);
-//		return null;
-//	}
-//}
+package com.battle.socket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class EchoCommandHandler {
+
+	@RequestMapping("/websocket/send")
+	public Object test()throws Exception{
+		
+		return null;
+	}
+}
--- battle-common/src/main/java/com/wyc/common/config/WebSocketConfig.java
+++ battle-common/src/main/java/com/wyc/common/config/WebSocketConfig.java
@@ -1,31 +1,27 @@
-//package com.wyc.common.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//import org.springframework.web.socket.config.annotation.EnableWebSocket;
-//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-//import org.springframework.web.socket.handler.TextWebSocketHandler;
-//
-///*
-//@Configuration
-//@EnableWebMvc
-//@EnableWebSocket
-//public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer{
-//
-//	@Override
-//	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//		
-//		registry.addHandler(webSocketHandler(),"/websocket/socketServer.do").addInterceptors(new SpringWebSocketHandlerInterceptor());
-//        registry.addHandler(webSocketHandler(), "/sockjs/socketServer.do").addInterceptors(new SpringWebSocketHandlerInterceptor()).withSockJS();
-//		
-//	}
-//	
-//	@Bean
-//    public TextWebSocketHandler webSocketHandler(){
-//        return new SpringWebSocketHandler();
-//    }
-//
-//}*/
+package com.wyc.common.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import com.wyc.common.config.scoket.CountWebSocketHandler;
+import com.wyc.common.config.scoket.HandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(ApplicationContext context) {
+        return new ServerEndpointExporter();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new CountWebSocketHandler(), "/web/count").addInterceptors(new HandshakeInterceptor());
+    }
+}
