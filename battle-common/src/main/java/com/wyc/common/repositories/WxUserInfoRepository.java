fe3d7ef6614396742ba07d77a7e10a592334db69
System Administrator <root@dawang.local>
2018-04-19 18:17:52 +0800

    commit

--- battle-common/src/main/java/com/wyc/common/repositories/WxUserInfoRepository.java
+++ battle-common/src/main/java/com/wyc/common/repositories/WxUserInfoRepository.java
@@ -1,6 +1,7 @@
 package com.wyc.common.repositories;
 import javax.persistence.QueryHint;
 
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
@@ -18,6 +19,9 @@ public interface WxUserInfoRepository extends CrudRepository<UserInfo, String>{
 	//@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Cacheable(value="userCache")
 	public UserInfo findOne(String id);
+	
+	@CachePut(value="userCache")
+	public UserInfo save(UserInfo userInfo);
 
 	@QueryHints({@QueryHint(name ="org.hibernate.cacheable", value ="true") })
 	public UserInfo findByOpenidAndSource(String openid, int source);
